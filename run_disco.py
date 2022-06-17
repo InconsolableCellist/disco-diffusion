5a2efb7cd344d82c414e8b9695ace1ea5fb13990
InconsolableCellist <shutup@nope.com>
2022-06-17 11:24:21 -0600

    Improving artist prompts

--- run_disco.py
+++ run_disco.py
@@ -10,7 +10,7 @@ settings = [
     "memes2.json"
 ]
 
-fav_artists = [
+artists_favorite = [
     "Tom Bagshaw", "Miyazaki", "Caspar David Friedrich",
     "Quentin Mabille", "Ivan Konstantinovich Aivazovsky","Makoto Shinkai", "Eugene Korolev"
     "Moebius", "laloux", "Carlos Ortega Elizalde"
@@ -23,47 +23,38 @@ artists_anime = [
 artists_illustration = [
     "fernando chamarelli", "matt mills", "Martin Johnson Heade"
 ]
-# artists = [
-#     "Leegan Koo", "Simon Stålenhag", "James Jean", "Boris Pelcer", "eugene korolev", "Olga Kim", "ismail inceoglu",
-#     "ulysse verhassel", "francisco martin", "henry wong", "axel sauerwald", "alexandr poda", "ingram schell",
-#     "mael ollivier henry", "ayan nag", "Ben Bauchau", "Syd Mead", "dariusz zawadzki",
-
-# artists = [ "huleeb", "frank mccarty",
-#     "Oscar Berninghaus", "Jim C Norton", "Thomas Cole", "makoto Shinkai", "Hiroshi Yoshida", "Sin jong hun", "Miyazaki",
-#     "Ivan Bilibin", "Studio Ghibli", "Cedric Peyravernay", "Peter Mohrbacher", ]
-
-# artists = [ "Tom Bagshaw", "Karol Bak",
-#     "Steve Argyle", "Hethe Srodawa", "Alena Aenami", "Philipp A. Urlich", "Tyler Edlin", "Anthony Efthekary",
-#     "marco gorlei", "cody foreman", "pavel vophira", "john sweeney", "roma likholob", "gilles ketting", "pete amachree",
-#     "sung choi", "victor hugo harmatiuk", "Min Guen", "Paul Chadeisson", "Jaime Jasso", "Jonathan Berube",
-#     "Steven Cormann", "Jaime Jasso", "Thu Berchs", "Terraform Studios", "Daniel Dana", "Erik van Helvoirt", "Luc Begin",
-#     "Sanhanat Suwanwised", "Hossein Diba", "Raf Grassetti", "Hadi Karimi", "Frank Tzeng", "Şefki Ibrahim",
-#     "Victor Hugo", "James Gurney", "Mark Simonetti", "Katsuhiro Otomo", "Yoshitaka Amano",
-#     "vance kovac", "Arnold Böcklin", "Fitz Hugh Lane", "Koen Wijffelaars", "Ian McQue", "Jan Urschel", "Craig Mullins",
-#     "Ivan Konstantinovich Aivazovsky", "nadia hurianova", "Luke berliner", "quentin mabille", "Liang Mark",
-#     "andrei riabovitchev", "john burton" "giorgi simeonov", "mathias zamecki", "Lloyd Allan", "John Stephens",
-#     "Peder Severin Krøyer", "albert Bierstadt", "Greg Rutkowski", "Caspar David Friedrich", "John Berkey",
-
-
-
-    # "Asher Brown Durand", "Nadezda Petrovic", "William Bradford", "Edvard Munch", "Vincent Van Gogh", "Gustav Klimt",
-    # "Claude Monet", "Eugene Delacroix", "Hieronymous Bosch", "Egon Schiele", "Henri de Toulouse-Lautrec",
-    # "Jean-Honoré Fragonard", "Jean-Michel Basquiat", "Android Jones", "Justin Totemical", "Kirsten Zirngibl",
-    # "Alex Grey", "andy gilmore", "jonathan solter", "ben ridgway", "kent baltutat", "Jake amason",
-    # "brian scott hampton", "CT Nelson", "Leszek Kostuj", "Rae Klein", "Yuri Yakovenko", "Eugenia Loli", "Dorris Vooijs",
-    # "Celine Ranger", "fernando chamarelli", "matt mills", "Martin Johnson Heade", "James Paick", "H R Giger",
-    # "John Harris" "Frederico Pelat", "JC Jongwon Park ", "Zeen Chin", "Ellen Jewett", "Nadezda sokolova", "Panamarenko",
-    # "kris kuksi", "Peter Elson", "John Berkey", "Roger Dean", "Pascal Blanché", "Mœbius", "Moebius", "Jean Giraud",
-    # "Sydney Jay Mead", "Kilian Eng", "Geof Darrow", "frank frazetta", "Bastien Lecouffe-Deharme", "Tuomas Korpi",
-    # "Daryl Mandryk", "Anato Finnstark", "noah bradley", "Aria Fawn", "Jeremy fenske", "leonid koliagin",
-    # "Sylvain Sarrailh", "Adam Burke", "Caelen Stokkermans", "Vex Van Hillik ", "Wiesław Wałkuski", "Akiya Kageichi",
-    # "Beeple", "Mike Winkelmann", "Filip Hodas", "Farid Ghanbari", "Billy Bogatzoglus", "Anwar Mostafa",
-    # "Stuart Lippincott", "Ari Weinkle", "Aleksandr Kuskov", "Gesy Bekeyei", "Andy Goralkcyzk", "Wes Anderson",
-    # "Liam Wong", "Bastien Grivet", "Stephen Shore", "Alphons Mucha", "Juras Rodionovas", "Soa Lee", "Ilya Kuvshinov",
-    # "Lucas Mendonça" "josan gonzalez", "Archan Nair", "Viktoria Gavrilenko", "Everett Kinstler", "KwangHo Shin",
-    # "Abdelrahman Taymour"
-
-
+artists = [
+    "Leegan Koo", "Simon Stålenhag", "James Jean", "Boris Pelcer", "eugene korolev", "Olga Kim", "ismail inceoglu",
+    "ulysse verhassel", "francisco martin", "henry wong", "axel sauerwald", "alexandr poda", "ingram schell",
+    "mael ollivier henry", "ayan nag", "Ben Bauchau", "Syd Mead", "dariusz zawadzki", "huleeb", "frank mccarty",
+    "Oscar Berninghaus", "Jim C Norton", "Thomas Cole", "makoto Shinkai", "Hiroshi Yoshida", "Sin jong hun", "Miyazaki",
+    "Ivan Bilibin", "Studio Ghibli", "Cedric Peyravernay", "Peter Mohrbacher", "Tom Bagshaw", "Karol Bak",
+    "Steve Argyle", "Hethe Srodawa", "Alena Aenami", "Philipp A. Urlich", "Tyler Edlin", "Anthony Efthekary",
+    "marco gorlei", "cody foreman", "pavel vophira", "john sweeney", "roma likholob", "gilles ketting", "pete amachree",
+    "sung choi", "victor hugo harmatiuk", "Min Guen", "Paul Chadeisson", "Jaime Jasso", "Jonathan Berube",
+    "Steven Cormann", "Jaime Jasso", "Thu Berchs", "Terraform Studios", "Daniel Dana", "Erik van Helvoirt", "Luc Begin",
+    "Sanhanat Suwanwised", "Hossein Diba", "Raf Grassetti", "Hadi Karimi", "Frank Tzeng", "Şefki Ibrahim",
+    "Victor Hugo", "James Gurney", "Mark Simonetti", "Katsuhiro Otomo", "Yoshitaka Amano", "vance kovac",
+    "Arnold Böcklin", "Fitz Hugh Lane", "Koen Wijffelaars", "Ian McQue", "Jan Urschel", "Craig Mullins",
+    "Ivan Konstantinovich Aivazovsky", "nadia hurianova", "Luke berliner", "quentin mabille", "Liang Mark",
+    "andrei riabovitchev", "john burton" "giorgi simeonov", "mathias zamecki", "Lloyd Allan", "John Stephens",
+    "Peder Severin Krøyer", "albert Bierstadt", "Greg Rutkowski", "Caspar David Friedrich", "John Berkey",
+    "Asher Brown Durand", "Nadezda Petrovic", "William Bradford", "Edvard Munch", "Vincent Van Gogh", "Gustav Klimt",
+    "Claude Monet", "Eugene Delacroix", "Hieronymous Bosch", "Egon Schiele", "Henri de Toulouse-Lautrec",
+    "Jean-Honoré Fragonard", "Jean-Michel Basquiat", "Android Jones", "Justin Totemical", "Kirsten Zirngibl",
+    "Alex Grey", "andy gilmore", "jonathan solter", "ben ridgway", "kent baltutat", "Jake amason",
+    "brian scott hampton", "CT Nelson", "Leszek Kostuj", "Rae Klein", "Yuri Yakovenko", "Eugenia Loli", "Dorris Vooijs",
+    "Celine Ranger", "fernando chamarelli", "matt mills", "Martin Johnson Heade", "James Paick", "H R Giger",
+    "John Harris" "Frederico Pelat", "JC Jongwon Park ", "Zeen Chin", "Ellen Jewett", "Nadezda sokolova", "Panamarenko",
+    "kris kuksi", "Peter Elson", "John Berkey", "Roger Dean", "Pascal Blanché", "Mœbius", "Moebius", "Jean Giraud",
+    "Sydney Jay Mead", "Kilian Eng", "Geof Darrow", "frank frazetta", "Bastien Lecouffe-Deharme", "Tuomas Korpi",
+    "Daryl Mandryk", "Anato Finnstark", "noah bradley", "Aria Fawn", "Jeremy fenske", "leonid koliagin",
+    "Sylvain Sarrailh", "Adam Burke", "Caelen Stokkermans", "Vex Van Hillik ", "Wiesław Wałkuski", "Akiya Kageichi",
+    "Beeple", "Mike Winkelmann", "Filip Hodas", "Farid Ghanbari", "Billy Bogatzoglus", "Anwar Mostafa",
+    "Stuart Lippincott", "Ari Weinkle", "Aleksandr Kuskov", "Gesy Bekeyei", "Andy Goralkcyzk", "Wes Anderson",
+    "Liam Wong", "Bastien Grivet", "Stephen Shore", "Alphons Mucha", "Juras Rodionovas", "Soa Lee", "Ilya Kuvshinov",
+    "Lucas Mendonça" "josan gonzalez", "Archan Nair", "Viktoria Gavrilenko", "Everett Kinstler", "KwangHo Shin",
+    "Abdelrahman Taymour"]
 
 # for artist in artists:
 #     p = {}
@@ -148,24 +139,20 @@ for x in range(1, 50):
                 p[key] = value
                 print(f'overriding {key} to {value}')
 
-        # If the prompt is a list we don't modify it at all, and we assume it's already fully formed
-        if not isinstance(prompt['prompt'], list):
-            if 'add_artist' in prompt and prompt['add_artist'] == True:
-                if 'artist_category' in prompt:
-                    if prompt['artist_category'] == "anime":
-                        new_prompt = [f'{prompt["prompt"]} by {random.choice(artists_anime)}, trending on Artstation:4']
-                    if prompt['artist_category'] == "illustration":
-                        new_prompt = [f'{prompt["prompt"]} by {random.choice(artists_illustration)}, trending on Artstation:4']
-                else:
-                    new_prompt = [ f'{prompt["prompt"]} by {random.choice(fav_artists)}, trending on Artstation:4' ]
+        if 'add_artist' in prompt and prompt['add_artist'] == True:
+            if 'artist_category' in prompt:
+                if prompt['artist_category'] == "anime":
+                    prompt['prompt'].append(f'by {random.choice(artists_anime)}:2')
+                if prompt['artist_category'] == "illustration":
+                    prompt['prompt'].append(f'by {random.choice(artists_illustration)}:2')
+                if prompt['artist_category'] == "favorites":
+                    prompt['prompt'].append(f'by {random.choice(artists_favorite)}:2')
             else:
-                new_prompt = [ f'{prompt["prompt"]}, trending on Artstation:4' ]
-            for c in common_prompts:
-                new_prompt.append(c)
-            p['text_prompts']['0'] = new_prompt
+                prompt['prompt'].append(f'by {random.choice(artists)}:2')
 
-        else:
-            p['text_prompts']['0'] = prompt['prompt']
+        for c in common_prompts:
+            prompt['prompt'].append(c)
+        p['text_prompts']['0'] = prompt['prompt']
 
         with open(settings[0], 'w') as f:
             json.dump(p, f)
